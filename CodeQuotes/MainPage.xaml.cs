dcf1bb368c3ec7f0bc5be2d15eb42c35cd34d943
KAleksandr <okuzmenkoa@gmail.com>
2023-07-02 21:14:05 +0300

    Adde logic generate random gradients and quotes

--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -1,14 +1,60 @@
-﻿namespace CodeQuotes;
+﻿using System;
+
+namespace CodeQuotes;
 
 public partial class MainPage : ContentPage
 {
-	
+    List<string> quotes = new List<string>();
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadMauiAsset();
+    }
+    Random random = new Random();
+    private void btnGenerateQuotes_Clicked(object sender, EventArgs e)
+    {
+       
+        var startColor = System.Drawing.Color.FromArgb(
+			random.Next(0,256),
+            random.Next(0, 256),
+            random.Next(0, 256));
+        var endColor = System.Drawing.Color.FromArgb(
+            random.Next(0, 256),
+            random.Next(0, 256),
+            random.Next(0, 256));
+        var colors =
+            ColorUtility
+            .ColorControls
+            .GetColorGradient(startColor, endColor,6);
+        float stopOffSet = .0f;
+        var stops = new GradientStopCollection();
+        foreach(var c in colors)
+        {
+            stops.Add(new GradientStop(Color.FromArgb(c.Name), stopOffSet));
+            stopOffSet += .2f;
+        }
+        var gradient = new LinearGradientBrush(stops,
+            new Point(0, 0),
+            new Point(1, 1));
+        background.Background = gradient;
+        int index = random.Next(0, quotes.Count-1);
+        lblquotes.Text = quotes[index];
+    }
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+        using var reader = new StreamReader(stream);
 
-	
+        while(reader.Peek() != -1)
+        {
+            quotes.Add(reader.ReadLine());
+        }
+        
+    }
 }
 
